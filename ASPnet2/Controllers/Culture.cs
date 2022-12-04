665862eb3b4c342828ed98cc825759170c2edfcd
Nekrohok <114496403+Nekrohok@users.noreply.github.com>
2022-12-04 19:54:09 +0200

    Добавьте файлы проекта.

---
+++ ASPnet2/Controllers/Culture.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASPnet2.Controllers
+{
+    public class Culture : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
